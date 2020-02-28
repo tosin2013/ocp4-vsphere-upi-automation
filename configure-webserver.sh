d5a7da9de5fe9d4e4e2061cc6c19558cc129b361
Tosin Akinosho <tosin.akinosho@gmail.com>
2020-02-28 17:12:08 -0500

    adding rsync

--- configure-webserver.sh
+++ configure-webserver.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 if [ -z $1 ]; then
   echo "Please pass user"
   echo "USAGE: ./$0 username"
@@ -10,8 +10,7 @@ chmod 0775 ${CURRENT_USER}/install-dir/*.ign
 cp -avi ${CURRENT_USER}/install-dir/*.ign /var/www/html/ignition/
 
 if [ -d ${CURRENT_USER}/ocp4-utils/ ]; then
-  chmod 0775 ${CURRENT_USER}/ocp4-utils/configFiles/*.com
-  cp -avi ${CURRENT_USER}/ocp4-utils/configFiles/*.com  /var/www/html/ignition/
+  rsync -aP --exclude="*.64"  ${CURRENT_USER}/ocp4-utils/configFiles/  /var/www/html/ignition/
 fi
 
 cd /var/www/html/ignition/
